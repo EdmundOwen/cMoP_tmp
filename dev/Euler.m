6b4408ce79551082f5376c606b1f7119898392d5
Edmund <eo13@hw.ac.uk>
2016-04-01 14:59:32 +0100

    Time step of memory functions in Euler now performed after density matrix evolution

--- dev/Euler.m
+++ dev/Euler.m
@@ -2,17 +2,17 @@
 
 function [mat, solution] = Euler(mat, solution, L, input, dt)
 
-    %% iterate the memory functions (if needed)
-    if input.memoryNeeded && ~input.isMemory
-        solution = IterateMemory(solution, input, dt);
-    end
-
     rhs = zeros(size(mat));
     %% calculate rhs of the Louiville equation
     for j = 1:numel(L)
         rhs = rhs + L{j}( input, mat, solution );
     end
-    
+  
+    %% iterate the memory functions (if needed)
+    if input.memoryNeeded && ~input.isMemory
+        solution = IterateMemory(solution, input, dt);
+    end
+  
     %% iterate the matrix
     mat = mat + dt * rhs;
     
