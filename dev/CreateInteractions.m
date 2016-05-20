256286ec99caba2752211d423933f30da0c4eee4
Edmund <eo13@hw.ac.uk>
2016-05-20 15:49:50 +0100

    Allow the option to tag system-environment interaction type

--- dev/CreateInteractions.m
+++ dev/CreateInteractions.m
@@ -15,17 +15,23 @@ function interactions = CreateInteractions( input, interaction_list )
         A_site_label = interaction_list{i}{2};
         A_site_operator = interaction_list{i}{3};
         B_site_label = interaction_list{i}{4};
-        B_site_operator = interaction_list{i}{5};    
-    
+        B_site_operator = interaction_list{i}{5};
+        % get the interaction type if it exists
+        if numel(interaction_list{i}) > 6
+            interaction_type = interaction_list{i}{7};
+        else % default is unitary
+            interaction_type = 'unitary';
+        end
+        
         % create system interaction operator
-        interaction_list{i}{7} = InsertOperator(A_site_operator, A_site_label, onsitedim, clustersize);
+        A_operator = InsertOperator(A_site_operator, A_site_label, onsitedim, clustersize);
         % create environment interaction operator 
         % (assumes that the system is translationally invariant)   
-        interaction_list{i}{8} = InsertOperator(B_site_operator, B_site_label, onsitedim, clustersize);
+        B_operator = InsertOperator(B_site_operator, B_site_label, onsitedim, clustersize);
         
         % input into interactions struct
-        interactions{i} = {interaction_list{i}{1}, interaction_list{i}{7}, ...
-                                    interaction_list{i}{8}, interaction_list{i}{6}};
+        interactions{i} = {interaction_list{i}{1}, A_operator, B_operator, ...
+                                interaction_list{i}{6}, interaction_type};
     end
 
 end
