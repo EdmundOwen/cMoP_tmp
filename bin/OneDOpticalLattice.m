107407cee76fbb4589f39bf2fe4567151f2cdf29
Edmund Owen <e.owen@hw.ac.uk>
2017-10-17 15:08:01 +0100

    Allow clustersize in OneDOpticalLattice.m greater than one

--- bin/OneDOpticalLattice.m
+++ bin/OneDOpticalLattice.m
@@ -6,32 +6,28 @@ tic
 
 %% physics
 hbar = 1.0;
-omega0 = 1.0;
-omega = 3.0;
+omega = 0.0;
 d_eg = 1.0;
-E = 0.0;
-Gamma_0 = 3.0;
+E = 0.1;
+Gamma_0 = 2.0;
 k0 = 1.0;
-dr = 100.0;
+dr = 1000.0;
+Delta = -0.5;
 
 %% parameters
 % system
-clustersize = 1;
+clustersize = 2;
 onsitedim = 2;
-L = { @L0, @LMF, @LBT };
-Delta = hbar * omega;
-gamma = Gamma_0;
-Omega = d_eg * E;
+L = { @L0, @LMF };
 
 % simulation
 method = 'heun';
-Nt = 200;
+Nt = 600;
 dt = 0.02;
 probelist = { @SaveDensity, @SaveMagnetisation, @SaveRho };
 
 %% setup
-setup_varargin = {'clustersize', clustersize, 'onsitedim', onsitedim, 'L', L, ...
-                    'Delta', Delta, 'gamma', gamma, 'Omega', Omega};
+setup_varargin = {'clustersize', clustersize, 'onsitedim', onsitedim, 'L', L};
 environ_varargin = {};
 timeiter_varargin = { 'method', method, 'Nt', Nt, 'dt', dt, 'probelist', probelist };
 steady_varargin = {};
@@ -43,11 +39,45 @@ input = SetupTimeIter(input, timeiter_varargin);
 rho = InitializeRho(input);
 
 %% create time dependent Hamiltonian
-energymat = -0.5 * hbar * omega0 * [1.0, 0.0; ...
-                                    0.0, -1.0];
-input.H0 = @(t) (energymat - [0.0, d_eg * E * exp(-1.0i * omega * t);...
-                            (d_eg * E * exp(-1.0i * omega * t))', 0.0]);
+a = annihilation(onsitedim);
+% create local Hamiltonians
+H0_loc = -0.5 * hbar * Delta * (a * a' - a' * a);
+H0_mat = TensorAddOperator(H0_loc, onsitedim, clustersize);
+% create unitary couplings within the partition
+if clustersize == 2
+    H0_mat = H0_mat + 0.5 * Gamma_0 * (besselj(0, k0 * dr) - 0.5 * besselj(2, k0 * dr)) * (kron(a, a') + kron(a', a));
+elseif clustersize > 2
+    throw exception;
+end
+% create time-dependent pumping terms
+Evec = zeros(1, clustersize);
+for j = 1:clustersize
+    Evec(j) = (-1.0)^j * E;
+end
+Epumpup = zeros(onsitedim^clustersize);
+Epumpdown = zeros(onsitedim^clustersize);
+for i = 1:clustersize
+    Epumpup = Epumpup + d_eg * Evec(i) * InsertOperator(a', i, onsitedim, clustersize);
+    Epumpdown = Epumpdown + (d_eg * Evec(i))' * InsertOperator(a, i, onsitedim, clustersize);
+end
+input.H0 = @(t) H0_mat - exp(-1.0i * omega * t) * Epumpup - exp(1.0i * omega * t) * Epumpdown;
+
 input.subinput{1}.H0 = input.H0;
+
+%% and create Lindblad operators
+A_Lindblad = cell(1, clustersize);
+Lindblad_weights = zeros(clustersize);
+for i = 1:clustersize-1
+    A_Lindblad{i} = InsertOperator(a, i, onsitedim, clustersize);
+    Lindblad_weights(i, i) = Gamma_0;
+    Lindblad_weights(i+1, i) = Gamma_0 * (bessely(0, k0 * dr) - 0.5 * bessely(2, k0 * dr));
+    Lindblad_weights(i, i+1) = Gamma_0 * (bessely(0, k0 * dr) - 0.5 * bessely(2, k0 * dr));
+end
+A_Lindblad{clustersize} = InsertOperator(a, clustersize, onsitedim, clustersize);
+Lindblad_weights(clustersize, clustersize) = Gamma_0;
+% and allocate them
+input.subinput{1}.A_Lindblad = A_Lindblad;
+input.subinput{1}.Lindblad_weights = Lindblad_weights;
 
 %% create interactions
 int_type = {};
@@ -58,20 +88,36 @@ for i = 1:interaction_range
     k0_r = k0 * i * dr;
     int_type{end+1} = containers.Map(keySet, {Gamma_0 * (bessely(0, k0_r) - 0.5 * bessely(2, k0_r)), ...
                                         struct('Index', 1, 'SiteOperator', a, 'SiteLabel', 1), ...
-                                        struct('Index', 1, 'SiteOperator', a, 'SiteLabel', 1), ...
-                                        [1 1 1 1], 2, 'dissipative_ARB'});
+                                        struct('Index', 1, 'SiteOperator', a, 'SiteLabel', clustersize), ...
+                                        [1 1 1 1 0 0 0 0], 1, 'dissipative_ARB'});
 	int_type{end+1} = containers.Map(keySet, {Gamma_0 * (bessely(0, k0_r) - 0.5 * bessely(2, k0_r)), ...
                                         struct('Index', 1, 'SiteOperator', a, 'SiteLabel', 1), ...
-                                        struct('Index', 1, 'SiteOperator', a, 'SiteLabel', 1), ...
-                                        [1 1 1 1], 2, 'dissipative_BRA'});
+                                        struct('Index', 1, 'SiteOperator', a, 'SiteLabel', clustersize), ...
+                                        [1 1 1 1 0 0 0 0], 1, 'dissipative_BRA'});
     int_type{end+1} = containers.Map(keySet, {0.5 * Gamma_0 * (besselj(0, k0_r) - 0.5 * besselj(2, k0_r)), ...
                                         struct('Index', 1, 'SiteOperator', a, 'SiteLabel', 1), ...
-                                        struct('Index', 1, 'SiteOperator', a', 'SiteLabel', 1), ...
-                                        [1 1 1 1], 2, 'unitary'});
+                                        struct('Index', 1, 'SiteOperator', a', 'SiteLabel', clustersize), ...
+                                        [1 1 1 1 0 0 0 0], 1, 'unitary'});
     int_type{end+1} = containers.Map(keySet, {0.5 * Gamma_0 * (besselj(0, k0_r) - 0.5 * besselj(2, k0_r)), ...
                                         struct('Index', 1, 'SiteOperator', a', 'SiteLabel', 1), ...
+                                        struct('Index', 1, 'SiteOperator', a, 'SiteLabel', clustersize), ...
+                                        [1 1 1 1 0 0 0 0], 1, 'unitary'});
+    int_type{end+1} = containers.Map(keySet, {Gamma_0 * (bessely(0, k0_r) - 0.5 * bessely(2, k0_r)), ...
+                                        struct('Index', 1, 'SiteOperator', a, 'SiteLabel', clustersize), ...
                                         struct('Index', 1, 'SiteOperator', a, 'SiteLabel', 1), ...
-                                        [1 1 1 1], 2, 'unitary'});
+                                        [0 0 0 0 1 1 1 1], 1, 'dissipative_ARB'});
+	int_type{end+1} = containers.Map(keySet, {Gamma_0 * (bessely(0, k0_r) - 0.5 * bessely(2, k0_r)), ...
+                                        struct('Index', 1, 'SiteOperator', a, 'SiteLabel', clustersize), ...
+                                        struct('Index', 1, 'SiteOperator', a, 'SiteLabel', 1), ...
+                                        [0 0 0 0 1 1 1 1], 1, 'dissipative_BRA'});
+    int_type{end+1} = containers.Map(keySet, {0.5 * Gamma_0 * (besselj(0, k0_r) - 0.5 * besselj(2, k0_r)), ...
+                                        struct('Index', 1, 'SiteOperator', a, 'SiteLabel', clustersize), ...
+                                        struct('Index', 1, 'SiteOperator', a', 'SiteLabel', 1), ...
+                                        [0 0 0 0 1 1 1 1], 1, 'unitary'});
+    int_type{end+1} = containers.Map(keySet, {0.5 * Gamma_0 * (besselj(0, k0_r) - 0.5 * besselj(2, k0_r)), ...
+                                        struct('Index', 1, 'SiteOperator', a', 'SiteLabel', clustersize), ...
+                                        struct('Index', 1, 'SiteOperator', a, 'SiteLabel', 1), ...
+                                        [0 0 0 0 1 1 1 1], 1, 'unitary'});
 end
 input.subinput{1}.interactions = CreateInteractions(input, int_type);
 
